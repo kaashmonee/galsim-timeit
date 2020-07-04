e8a89c3af8b58ad936fa0264799f58e759cf602c
Skanda Kaashyap <skandakk@gmail.com>
2020-07-04 17:23:41 -0500

    Updated main documentation

--- timer/main.py
+++ timer/main.py
@@ -1,8 +1,14 @@
 from core import Timer
 
 def main():
+    """
+    An example main routine. This main function should function as a 
+    series of examples of how to use this utility. It also serves
+    to test all the routines and plot them to ensure that they are 
+    working properly. This can be changed to suit your needs.
+    """
 
-    # Simply create a timer for a galaxy with a specicfic PSF
+
     start, end = 1.e3, 1.e7
     exp_kolm_t = Timer("exponential", (start, end), debug=True)
     exp_kolm_t.time_init()
