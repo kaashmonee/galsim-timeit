a7f7b79cd2e9fc66518f7ee3a45673bc3ab6815d
Skanda Kaashyap <skandakk@gmail.com>
2020-07-21 17:14:45 -0500

    Got rid of extraneous function

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -9,24 +9,6 @@ class Experiment:
     This class will store all the experiments.
     """
 
-    def plot_setup_times(self):
-        start, end = 1.e3, 1.e5
-
-        for galaxy in Timer.GALAXY_NAMES:
-            fig, axs = plt.subplots(1, 2)
-
-            for psf in Timer.PSFS:
-                t = Timer(galaxy, (start, end), debug=False)
-                t.time_init()
-                t.plot_init_times(axis=axs[0])
-
-                t.set_psf(psf)
-                t.compute_phot_draw_times()
-                t.plot_draw_times(axis=axs[1])
-
-            plt.show()
-
-
     def time_phot_shooting_vs_gal_size(self):
         """
         Experiment: Time to do photon shooting while varying the galaxy
